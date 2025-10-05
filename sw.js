d371f9ac6da49eb5d485a0d76e2acaa041c9bc16
shingo20 <shenwujiaodao@gmail.com>
2025-10-05 11:21:44 +0900

    ver3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// シンプルなキャッシュ戦略（PWA用）
+// キャッシュ戦略（オフライン対応）
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("app-cache").then((cache) => {
@@ -13,4 +13,13 @@ self.addEventListener("fetch", (event) => {
       return resp || fetch(event.request);
     })
   );
+});
+
+// 通知クリックで再度タブを開けるように
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  const url = event.notification.data.url;
+  if (url) {
+    event.waitUntil(clients.openWindow(url));
+  }
 });
