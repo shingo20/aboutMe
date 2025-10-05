2f6094d195c7348e8798ea9656166cc981c9116a
shingo20 <shenwujiaodao@gmail.com>
2025-10-05 11:13:20 +0900

    ver2

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("app-cache").then(cache => {
-    return cache.addAll(["./","./index.html","./manifest.webmanifest"]);
-  }));
+// シンプルなキャッシュ戦略（PWA用）
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/manifest.webmanifest"]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
   );
 });
